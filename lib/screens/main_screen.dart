b4420f714ce75a80b5d38695820ce56b8ac5a520
misericordiagd <mesegirones97@gmail.com>
2019-12-04 10:23:48 +0100

    Sort levels by stages

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -13,35 +13,35 @@ class MainScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: stages.length,
         itemBuilder: (ctx, stage) => Container(
-          height: 150,
-          width: 150,
-          //  constraints: BoxConstraints.expand(width: 50.0, height: 100.0),
-          child: Stack(
-            alignment: AlignmentDirectional.center,
-            children: <Widget>[
-              ListView.builder(
-                itemCount: stages[stage].length,
-                itemBuilder: (ctx, level) => Container(
-                  height: 50,
-                  width: 50,
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  // color: Colors.blue[100 * stage],
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: FlatButton(
-                      child: Text('stage: $stage level: $level'),
-                      onPressed: () {
-                        Navigator.of(context).pushNamed(
-                          './operation',
-                          arguments: stage,
-                        );
-                      },
-                      color: Colors.blue[100 * stage],
-                    ),
-                  ),
+          height: stages[stage].length * 110.0,
+          width: double.infinity,
+          child: ListView.builder(
+            // shrinkWrap: true,
+            //  physics: ScrollPhysics(),
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: stages[stage].length,
+            itemBuilder: (ctx, level) => Align(
+              alignment: Alignment.center,
+              child: Container(
+                height: 100,
+                width: 100,
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                // color: Colors.green[200],
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                child: FlatButton(
+                  child: Text('stage: $stage level: $level'),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(
+                      './operation',
+                      arguments: stage,
+                    );
+                  },
+                  color: Colors.blue[100 * stage],
+                ),
                 ),
               ),
-            ],
+            ),
           ),
         ),
       ),
